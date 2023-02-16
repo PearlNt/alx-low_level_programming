2e8fcac76c3b5cef2cb15548e94486ba107eb1b8
PearlNt <pearleenmpanza0@gmail.com>
2023-02-16 12:18:15 -0800

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <studio.h>
-/*
- * main - print a line of code using puts
+/**
+ *  main - a program thats prints a line with puts functions
  *
- * Return:  0
- */
- int main (void) 
- {
-         puts("\|"Programming is like building a multilingual puzzle");
-	 return (0) ;
- ]
+ * return: 0(success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
